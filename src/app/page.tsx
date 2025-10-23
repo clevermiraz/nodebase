721aa7d166b05c865cc745379041a065e35bbcec
clevermiraz <miraz.onesoft@gmail.com>
2025-10-23 07:09:48 +0600

    02: Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
   return (
     <div>
       <Button>Click me</Button>
+
+      {JSON.stringify(users)}
     </div>
   );
 }
